84f6b223421ead0eb13ef06b84e9cbd5bee37940
DifferenTilt <andrea.brambilla@dodifferent.it>
2025-06-16 15:10:19 +0200

    package part
    keep an eye on package declaration

---
+++ chapter8/bookpack/Book.java
@@ -0,0 +1,21 @@
+package chapter8.bookpack;
+
+public class Book {
+	private String title;
+	private String author;
+	private int pubDate;
+	
+	public Book(String title, String author, int pubDate) {
+		this.title = title;
+		this.author = author;
+		this.pubDate = pubDate;
+	}
+	
+	public void show() {
+		System.out.println(title);
+		System.out.println(author);
+		System.out.println(pubDate);
+		System.out.println();
+	}
+	
+}
---
+++ chapter8/bookpack/BookDemo.java
@@ -0,0 +1,20 @@
+// A short package demonstration.
+package chapter8.bookpack;
+
+class BookDemo {
+
+	public static void main(String[] args) {
+		Book books[] = new Book[5];
+
+		books[0] = new Book("Java: A Beginner's Guide","Schildt", 2014);
+		books[1] = new Book("Java: The Complete Reference","Schildt", 2014);
+		books[2] = new Book("The Art of Java","Schildt and Holmes", 2003);
+		books[3] = new Book("Red Storm Rising","Clancy", 1986);
+		books[4] = new Book("On the Road","Kerouac", 1955);
+		
+		for (Book book : books) {
+			book.show();
+		}
+	}
+
+}
