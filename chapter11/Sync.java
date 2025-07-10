ec61b77c7e04cbd4909159708e7b2c6117180955
Andrea Brambilla <andrea.brambilla@dodifferent.it>
2025-07-10 09:05:58 +0200

    Reformat array and comments in Sync.java
    Adjusted spacing in array initialization and comments for consistency and improved readability in the Sync.java file.

--- chapter11/Sync.java
+++ chapter11/Sync.java
@@ -5,13 +5,13 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
 public class Sync {
 
 	public static void main(String[] args) {
-		int a[] = {1, 2 , 3, 4, 5, 6, 89, 32};
+		int a[] = { 1, 2, 3, 4, 5, 6, 89, 32 };
 		AtomicIntegerArray b = new AtomicIntegerArray(a);
-		
+
 		MyThread4 mt1 = new MyThread4("Child #1", b);
 		MyThread4 mt2 = new MyThread4("Child #2", b);
-		
-		// We use join() to tell main to wait thread to end before continuing with 
+
+		// We use join() to tell main to wait thread to end before continuing with
 		// the execution
 		try {
 			mt1.thread.join();
