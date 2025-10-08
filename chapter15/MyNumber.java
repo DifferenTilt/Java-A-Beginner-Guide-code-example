afb587469a656848c02c6cd3ae912efd54496706
Andrea Brambilla <andrea.brambilla@dodifferent.it>
2025-10-08 09:44:48 +0200

    Add MyNumber functional interface example
    Introduces the MyNumber interface in chapter15 as an example of a functional interface with a single abstract method, getValue().

---
+++ chapter15/MyNumber.java
@@ -0,0 +1,10 @@
+package chapter15;
+/**
+ * This is an example of a functional interface where we have only one
+ * abstract method. The method here is implicitly abstract. Here MyNumber is
+ * a functional Interface and its function is defined by getValue
+ */
+public interface MyNumber {
+
+	double getValue();
+}
